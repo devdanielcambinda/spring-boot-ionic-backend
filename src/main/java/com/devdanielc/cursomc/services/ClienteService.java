79e2f5b786733a533da88e994285ccc65686e9e4
Daniel Cambinda <danielcambinda@gmail.com>
2021-08-28 11:24:20 +0100

    Update: @Transactional no método insert e usar objeto cidade em vez de cidadeRepository

--- src/main/java/com/devdanielc/cursomc/services/ClienteService.java
+++ src/main/java/com/devdanielc/cursomc/services/ClienteService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devdanielc.cursomc.domain.Cidade;
 import com.devdanielc.cursomc.domain.Cliente;
@@ -40,6 +41,7 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
 		obj = repo.save(obj);
@@ -80,7 +82,7 @@ public class ClienteService {
 		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
 				TipoCliente.toEnum(objDto.getTipo()));
 
-		Cidade cid = cidadeRepository.findById(objDto.getCidadeId()).get();
+		Cidade cid = new Cidade(objDto.getCidadeId(),null,null);
 
 		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
 				objDto.getBairro(), objDto.getCep(), cli, cid);
